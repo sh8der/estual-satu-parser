4d9aa8ab0f137b635e881e8b81638cf58a68c1a4
Кирилл <sh8der@mail.ru>
2020-02-01 17:05:25 +0600

    init
    init

---
+++ indexjs.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const cherio = require("cheerio");
+const productListPage = "https://estual.satu.kz/product_list";
+
+let getBrandFilterID = pageHtml => {
+	const $ = cherio.load(pageHtml);
+	IDList = [];
+	$(".cs-manufactures-list__item a").forEach(a =>
+		IDList.push(a.href.split("=")[1])
+	);
+};
