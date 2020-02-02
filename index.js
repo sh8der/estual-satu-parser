ac83cdc0d5ebe7260e727bb3bd3cd0524bdea021
Кирилл <sh8der@mail.ru>
2020-02-03 01:46:26 +0600

    get all link
    Получаем все ссылки на страницы с продуктами

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const queue = require('async/queue');
+
 const productListPageUrl = "https://estual.satu.kz/product_list";
+let allProductLinks = [];
 
 let getBrandFilterID = html => {
 	const $ = cheerio.load(html);
@@ -9,10 +12,41 @@ let getBrandFilterID = html => {
 	$(".cs-manufactures-list__item a").each((i, a) => {
 		idList.push($(a).attr('href').split("=")[1]);
 	})
-	return productListPageUrl + "/page_1?" + idList.map(e => "bss0=" + e).join("&");
+	return idList.map(e => "bss0=" + e).join("&");
 };
+
+let q = queue(function(link, callback) {
+	axios.get(link).then(r => {
+		$ = cheerio.load(r.data);
+		let tempData = []
+		$('.cs-product-gallery__list .cs-product-gallery__item a.cs-product-gallery__image-link').each((i,e) => {
+			let t = $(e).attr('href');
+			console.log(t)
+			allProductLinks.push(t);
+		})
+		callback();
+	});
+}, 10);
+
+q.drain(function() {
+		console.log('all items have been processed');
+		console.log(allProductLinks);
+});
+
+// assign an error callback
+q.error(function(err, task) {
+		console.error('task experienced an error');
+});
 
 (async()=>{
 	let productPage = await axios.get(productListPageUrl);
-	console.log( getBrandFilterID(productPage.data) );
+	let idFilterList = getBrandFilterID(productPage.data);
+
+	productPage = await axios.get(productListPageUrl + "?" + idFilterList);
+	let $ = cheerio.load(productPage.data);
+	let productPageCount = Number($('.b-pager__dotted-link').next().text());
+	for (let i = 1; i <= productPageCount; i++) {
+		q.push(`${productListPageUrl}/page_${i}?${idFilterList}`);
+	}
+	
 })();
